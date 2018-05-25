19b4450bd2e82161e9e28821531346acaa683608
league <league@imac.attlocal.net>
2018-05-24 19:57:09 -0700

    My first swing GUI, Dueling buttons, Drum kit

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -12,60 +13,70 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DuelingButtons implements ActionListener {
-	
+
 	public static void main(String[] args) {
 		new DuelingButtons().createUI();
 	}
 
 	JButton leftButton = new JButton();
 	JButton rightButton = new JButton();
-	
-	Dimension BIG = new Dimension(400,400);
-	Dimension SMALL = new Dimension(200,200);
-	
+
+	Dimension BIG = new Dimension(400, 400);
+	Dimension SMALL = new Dimension(200, 200);
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+		leftButton.setText("Click Me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+		rightButton.setText("Click Me!");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+		frame.setTitle("DemandingButtons");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-		
-		
-		/* If the buttonPressed was the leftButton....*/
-			// Set the text of the rightButton to "No, click Me!"
-			// Set the PREFERRED size of the rightButton to BIG
-			// Set the text of the leftButton to "Click Me!"
-			// Set the PREFERRED size of the leftButton to SMALL
-		
-		
-		/* If the buttonPressed was the rightButton, do the opposite. */
-		
 
+		/* If the buttonPressed was the leftButton.... */
+		if (buttonPressed.equals(leftButton)) {
+			// Set the text of the rightButton to "No, click Me!"
+			rightButton.setText("No, Click ME!");
+			// Set the PREFERRED size of the rightButton to BIG
+			rightButton.setPreferredSize(BIG);
+			// Set the text of the leftButton to "Click Me!"
+			leftButton.setText("Click Me!");
+			// Set the PREFERRED size of the leftButton to SMALL
+			leftButton.setPreferredSize(SMALL);
+		}
+
+		/* If the buttonPressed was the rightButton, do the opposite. */
+		if (buttonPressed.equals(rightButton)) {
+			// Set the text of the leftButton to "No, click Me!"
+			leftButton.setText("No, Click ME!");
+			// Set the PREFERRED size of the leftButton to BIG
+			leftButton.setPreferredSize(BIG);
+			// Set the text of the rightButton to "Click Me!"
+			rightButton.setText("Click Me!");
+			// Set the PREFERRED size of the rightButton to SMALL
+			rightButton.setPreferredSize(SMALL);
+		}
 		frame.pack();
 	}
 }
-
-
--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -14,40 +13,40 @@ import javax.swing.JPanel;
 
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
-		//1. Create and initialize an object of the JFrame class
+		// 1. Create and initialize an object of the JFrame class
+		JFrame paintbrush = new JFrame();
+		// 2. Set your JFrame object to be visible
+		paintbrush.setVisible(true);
+		// 3. Run your program. Do you see your window? It's probably very small.
 
-		//2. Set your JFrame object to be visible
+		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
+		paintbrush.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 5. Create and initialize an object of the JPanel class
+		JPanel toothbrush = new JPanel();
+		// 6. Create and initialize an object of the JLabel class
+		JLabel airbrush = new JLabel();
+		// 7. Set the text of the JLabel to a lovely greeting.
+		airbrush.setText("Hi, how are you? It's a nice day!");
+		// 8. Add the JPanel object to the JFrame
+		paintbrush.add(toothbrush);
+		// 9. Add the JLabel object to the JPanel
+		toothbrush.add(airbrush);
+		// 10. Pack your JFrame.
+		paintbrush.pack();
+		// 11. Run your program again. Do you see your message.
 
-		//3. Run your program. Do you see your window? It's probably very small.
-		
-		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
-		//5. Create and initialize an object of the JPanel class
-
-		//6. Create and initialize an object of the JLabel class
-
-		//7. Set the text of the JLabel to a lovely greeting.
-
-		//8. Add the JPanel object to the JFrame
-
-		//9. Add the JLabel object to the JPanel
-
-		//10. Pack your JFrame.
-
-		//11. Run your program again. Do you see your message.
-		
-		//12. Use the loadImage method to set the icon of the JLabel object.  
-
-		//13. Re-pack the JFrame object.
-		
-		//14. Run the program one more time. Do you see the image?
+		// 12. Use the loadImage method to set the icon of the JLabel object.
+		airbrush.setIcon(loadImage());
+		// 13. Re-pack the JFrame object.
+		paintbrush.pack();
+		// 14. Run the program one more time. Do you see the image?
 	}
-	
-	public static ImageIcon loadImage(){
+
+	public static ImageIcon loadImage() {
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
 		} catch (IOException e) {
-			
+
 			return null;
 		}
 	}
